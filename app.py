d5789bd4e8930b9c89b54d52f0c848e0f60f93ca
Jose Salvatierra <jslvtr@gmail.com>
2019-07-04 15:00:23 +0100

    Add more required imports and pass a value into the homepage.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Want to find out how we'd complete it?
 Check out our blog post: <>
 """
 
-from flask import Flask, render_template, session
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = "jose"
@@ -16,13 +16,11 @@ users = {"jose": ("jose", "1234")}
 
 @app.route("/")
 def home():
-    return render_template("home.jinja2")
+    return render_template("home.jinja2", name=session.get("username", "Unknown"))
 
 
 @app.route("/login")
 def login():
-    # Here you could deal with the user's username and password.
-    # Check if they're correct.
     return render_template("login.jinja2")
 
 
